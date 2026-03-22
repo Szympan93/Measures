a73844a461e5a918b08c90896794a01a5f4a5814
Szymon Migowski <szymon.m93@gmail.com>
2026-03-22 14:46:42 +0100

    Add operators and Equals, GetHashCode to Mass

--- Runtime/Mass.cs
+++ Runtime/Mass.cs
@@ -18,9 +18,44 @@ namespace FunFact.Measures
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Mass FromGrams(float grams) => new(grams);
 
+        public override int GetHashCode() => _raw.GetHashCode();
         public int CompareTo(object obj) => obj is Mass other ? CompareTo(other) : 0;
         public int CompareTo(Mass other) => _raw.CompareTo(other._raw);
         public bool Equals(Mass other) => _raw.Equals(other._raw);
+        public override bool Equals(object obj) => obj is Mass other && Equals(other);
         public string ToString(string format, IFormatProvider formatProvider) => $"{_raw.ToString(format, formatProvider)}g";
+                
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Mass operator+(Mass a, Mass b) => new(a._raw + b._raw);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Mass operator-(Mass a, Mass b) => new(a._raw - b._raw);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Mass operator*(Mass a, float b) => new(a._raw - b);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Mass operator/(Mass a, float b) => new(a._raw - b);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Mass operator-(Mass a) => new(-a._raw);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(Mass a, Mass b) => a._raw == b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(Mass a, Mass b) => a._raw != b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator >(Mass a, Mass b) => a._raw > b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator <(Mass a, Mass b) => a._raw < b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator >=(Mass a, Mass b) => a._raw >= b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator <=(Mass a, Mass b) => a._raw <= b._raw;
     }
 }
