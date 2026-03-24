5e423a1d147f39b4aa7576c34fd74afd48c9c0dd
Szymon Migowski <szymon.m93@gmail.com>
2026-03-24 01:38:54 +0100

    Add Length type

---
+++ Runtime/Length.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Runtime.CompilerServices;
+
+namespace FunFact.Measures
+{
+    public readonly struct Length : IComparable, IComparable<Length>, IEquatable<Length>, IFormattable
+    {
+        private readonly float _raw;
+        
+        public float Meters
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _raw;
+        }
+        
+        private Length(float raw) => _raw = raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length FromMeters(float meters) => new(meters);
+
+        public override int GetHashCode() => _raw.GetHashCode();
+        public int CompareTo(object obj) => obj is Length other ? CompareTo(other) : 0;
+        public int CompareTo(Length other) => _raw.CompareTo(other._raw);
+        public bool Equals(Length other) => _raw.Equals(other._raw);
+        public override bool Equals(object obj) => obj is Length other && Equals(other);
+        public string ToString(string format, IFormatProvider formatProvider) => $"{_raw.ToString(format, formatProvider)}m";
+                
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length operator+(Length a, Length b) => new(a._raw + b._raw);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length operator-(Length a, Length b) => new(a._raw - b._raw);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length operator*(Length a, float b) => new(a._raw - b);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length operator/(Length a, float b) => new(a._raw - b);
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length operator-(Length a) => new(-a._raw);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator ==(Length a, Length b) => a._raw == b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator !=(Length a, Length b) => a._raw != b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator >(Length a, Length b) => a._raw > b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator <(Length a, Length b) => a._raw < b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator >=(Length a, Length b) => a._raw >= b._raw;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator <=(Length a, Length b) => a._raw <= b._raw;
+    }
+}
