8a1b107a80bbd997e682cdb068ed8de4b67e8899
Szymon Migowski <szymon.m93@gmail.com>
2026-03-24 01:42:11 +0100

    Add syntactic sugar converters for Length

---
+++ Runtime/LengthExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace FunFact.Measures
+{
+    public static class LengthExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length Meters(this double value) => Length.FromMeters((float)value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length Meters(this float value) => Length.FromMeters(value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length Meters(this long value) => Length.FromMeters(value);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Length Meters(this int value) => Length.FromMeters(value);
+    }
+}
