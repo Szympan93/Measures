0b59d296444ed95c5bf43048bd102d43378a5fb7
Szymon Migowski <szymon.m93@gmail.com>
2026-03-26 23:50:55 +0100

    Add SiPrefixData struct

---
+++ Runtime/SiPrefixData.cs
@@ -0,0 +1,9 @@
+﻿namespace FunFact.Measures
+{
+    public readonly struct SiPrefixData
+    {
+        public readonly SiPrefix SiPrefix;
+        public readonly float Scale;
+        public readonly string Symbol;
+    }
+}
